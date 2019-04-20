734a4b330941bd260218bd8adf48f5ba3ea24a09
pingjui <pingjuiliao@gmail.com>
2019-04-19 21:12:08 -0700

    add a lot

--- debian.sh
+++ debian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y build-essential vim python python-pip
-
-sudo apt install qemu
+sudo apt install -y build-essential vim python python-pip tmux
+sudo apt install -y gcc-multilib
+sudo apt install -y qemu clang
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #### amix's awesome version of amix
+sudo locale-gen zh_TW.UTF-8
 
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
