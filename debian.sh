a0389b4adc66b7628f7db3eb736e806d46842e19
pingjui <pingjuiliao@gmail.com>
2019-04-20 19:00:08 -0700

    add sth

--- debian.sh
+++ debian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y build-essential vim python python-pip tmux
-sudo apt install -y gcc-multilib
 sudo apt install -y qemu clang
+sudo apt install -y libc6-dev libc6-dev-i386 gcc-multilib g++-multilib clang cmake
